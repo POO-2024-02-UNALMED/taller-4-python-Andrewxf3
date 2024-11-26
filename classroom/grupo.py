fd9cb9329a1314d7e8ac306a5d0818f95cdb3a88
Andrés Felipe Muñoz Ortiz <163070299+Andrewxf3@users.noreply.github.com>
2024-11-26 02:17:30 +0000

    3

--- classroom/grupo.py
+++ classroom/grupo.py
@@ -8,19 +8,19 @@ class Grupo:
         self._asignaturas = asignaturas if asignaturas is not None else []
         self.listadoAlumnos = estudiantes if estudiantes is not None else []
 
-    def listadoAsignaturas(self, kwargs):
-        for x in kwargs.values():
-            self._asignaturas.append(Asignatura(x, "remoto"))
+    def listadoAsignaturas(self, **kwargs):
+        for key, value in kwargs.items():
+            self._asignaturas.append(Asignatura(value))
 
     def agregarAlumno(self, alumno, lista=None):
         if lista is None:
             lista = []
         lista.append(alumno)
-        self.listadoAlumnos = self.listadoAlumnos + lista
+        self.listadoAlumnos.extend(lista)
 
     def __str__(self):
         return f"Grupo de estudiantes: {self._grupo}"
 
     @classmethod
-    def asignarNombre(cls, nombre="Grado 10"):
+    def asignarNombre(cls, nombre="Grado 6"):
         cls.grado = nombre
